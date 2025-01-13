81e6ab20d9ac1fe1764a8f8466dafd356aaddf67
Dennis Morello <dennismorello@gmail.com>
2025-01-13 16:44:31 +0100

    chore: fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,26 @@
+import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  includeIgnoreFile(gitignorePath),
+  {
+    files: ["astro.config.mjs"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 ];
